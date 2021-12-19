e5a3e3e535126126443192a74aaf29c1b3f157bb
foptun <oixuier.toppfun@gmail.com>
2021-12-20 00:21:23 +0700

    Create Ex11.cpp

---
+++ Ex11.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    
+    int sum = 0;
+    
+    for (int i = 1; i <= 10 ; i++) {
+        
+        int x = 0;
+        printf("Enter Number (%d): ", i);
+        scanf("%d", &x);
+        
+        sum = sum + x;
+    }
+    
+    float av = ((float)sum / 10);
+    
+    printf("The sum of these integers is: %d\n", sum);
+    printf("The average of these integers is: %.2f", av);
+    
+    return 0;
+}
