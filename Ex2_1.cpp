9abc4b4ef6364e59f4ab3fc3099e4c0029dad26a
foptun <oixuier.toppfun@gmail.com>
2021-12-19 22:01:28 +0700

    Create Ex2_1.cpp

---
+++ Ex2_1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int x = 0;
+    printf("Enter a Number: ");
+    scanf("%d", &x);
+    
+    for (int i = 0; i < 8 ; i++) {
+        printf("*");
+    }
+    
+    
+    return 0;
+}
